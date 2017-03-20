b6e6748bc8d1af5ff6cde614068923f96b1ea317
weightedEights <poormanjosh@gmail.com>
2017-03-20 12:47:26 -0300

    Initial commit

---
+++ 01.HelloWorld/program.py
@@ -0,0 +1,9 @@
+print("---------------------------------")
+print("         Hello World")
+print("---------------------------------")
+
+greeting = "Nice to meet you "
+userText = input("What is your name? ")
+
+print(greeting + userText + "!")
+
