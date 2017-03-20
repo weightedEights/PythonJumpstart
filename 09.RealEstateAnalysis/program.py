cff02aec83cda9a20be94a16fd1bf994e7d55efd
weightedEights <poormanjosh@gmail.com>
2017-03-20 16:18:36 -0300

    added real estate data, added subdirectory /data

--- 09.RealEstateAnalysis/program.py
+++ 09.RealEstateAnalysis/program.py
@@ -19,16 +19,17 @@ def printHeader():
 
 def getDataFile():
     baseFolder = os.path.dirname(__file__)
-    return os.path.join(baseFolder, 'data', 'dataFile.csv')
+    return os.path.join(baseFolder, 'data', 'SacramentoRealEstateTransactions2008.csv')
 
 
 def loadData(datFile):
     with open(datFile, 'r', encoding='utf-8') as fin:
         reader = csv.DictReader(fin)
+        print(reader)
 
 
 def queryData(loadedData):
-    pass
+    return loadedData
 
 
 if __name__ == '__main__':
