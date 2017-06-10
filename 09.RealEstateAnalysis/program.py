8feaddc29fa9d8f58be64e5b37fd91ff09f9088c
weightedEights <poormanjosh@gmail.com>
2017-06-10 15:58:28 -0200

    reverted loadData to return instead of yield. added queryData sorting, high and low purchases.

--- 09.RealEstateAnalysis/program.py
+++ 09.RealEstateAnalysis/program.py
@@ -25,10 +25,21 @@ def getDataFile():
 
 def loadData(datFile):
     with open(datFile, 'r', encoding='utf-8') as fin:
-        yield [Purchase.createFromDict(row) for row in csv.DictReader(fin)]
+        return [Purchase.createFromDict(row) for row in csv.DictReader(fin)]
 
 
 def queryData(loadedData):
+
+    loadedData.sort(key=lambda p: p.price)
+
+    high_purchase = loadedData[-1]
+    print("The most expensive house is ${:,} with {} beds and {} baths.".format(
+        high_purchase.price, high_purchase.beds, high_purchase.baths))
+
+    low_purchase = loadedData[0]
+    print("The least expensive house is ${:,} with {} beds and {} baths.".format(
+        low_purchase.price, low_purchase.beds, low_purchase.baths))
+
     return loadedData
 
 
